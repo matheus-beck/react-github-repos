83fe043446ac273c534787e871472ec727673f05
matheus-beck <matheus.alencarbeck@gmail.com>
2020-02-27 21:26:24 -0300

    Updates

---
+++ precache-manifest.88d7dc53c8151c5042cb4a9437181bda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af770610b118037f2d34fbfcd7ce41f5",
+    "url": "/react-github-repos/index.html"
+  },
+  {
+    "revision": "199af262e383d06c44f3",
+    "url": "/react-github-repos/static/js/2.cfa813fe.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-github-repos/static/js/2.cfa813fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "815550728d4f7b1df661",
+    "url": "/react-github-repos/static/js/main.cb0dc58a.chunk.js"
+  },
+  {
+    "revision": "ec433cca593b895db055",
+    "url": "/react-github-repos/static/js/runtime-main.72865abe.js"
+  }
+]);
